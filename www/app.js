97055bd0345f9fab8b535572d348ecc21228f69b
majidghol <giba.gibson87@gmail.com>
2026-06-06 05:54:23 +0330

    Improve UI

--- www/app.js
+++ www/app.js
@@ -1,8 +1,16 @@
+
 const note = document.getElementById("note");
+const message = document.getElementById("message");
 
 note.value = localStorage.getItem("note") || "";
 
 function saveNote(){
 localStorage.setItem("note", note.value);
-alert("ذخیره شد");
+
+message.textContent = "✅ یادداشت ذخیره شد";
+
+setTimeout(() => {
+    message.textContent = "";
+}, 2000);
+
 }
